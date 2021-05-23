7d4d33acc58e690fa1fdc1e77823d086ba24f891
Daniel Amie <amiedaniel92@gmail.com>
2021-05-23 23:41:37 +0100

    add minor changes

--- client/src/components/PostDetails/styles.js
+++ client/src/components/PostDetails/styles.js
@@ -4,21 +4,28 @@ export default makeStyles((theme) => ({
   media: {
     borderRadius: "20px",
     objectFit: "cover",
-    width: "50%",
     maxHeight: "600px",
+    [theme.breakpoints.down("sm")]: {
+      width: "80%",
+      objectFit: "contain",
+    },
   },
   card: {
     display: "flex",
     width: "100%",
-    [theme.breakpoints.down("sm")]: {
+    [theme.breakpoints.down("md")]: {
       flexWrap: "wrap",
       flexDirection: "column",
+      textAlign: "center",
     },
   },
   section: {
     borderRadius: "20px",
-    margin: "10px",
+    margin: "50px",
     flex: 1,
+    [theme.breakpoints.down("sm")]: {
+      textAlign: "center",
+    },
   },
   imageSection: {
     marginLeft: "20px",
@@ -30,12 +37,13 @@ export default makeStyles((theme) => ({
     display: "flex",
     [theme.breakpoints.down("sm")]: {
       flexDirection: "column",
+      textAlign: "center",
     },
   },
   loadingPaper: {
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "center", // L-R
+    alignItems: "center", // Up - Down
     padding: "20px",
     borderRadius: "15px",
     height: "39vh",
